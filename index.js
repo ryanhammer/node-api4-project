6c91ffc8d174c9e78bbdb4e0b135b2e1ef0807d2
rhammer35 <rhammer35@icloud.com>
2021-05-06 14:12:48 -0400

    removed front end app files

--- index.js
+++ index.js
@@ -1,17 +1,9 @@
 require('dotenv').config();
-const path = require('path');
-const express = require('express');
 
 // require your server and launch it
 const server = require('./api/server');
 
 const port = process.env.PORT;
-
-server.use(express.static(path.join(__dirname, 'client/build')));
-
-server.use('*', (_, res) => {
-  res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
-})
 
 server.listen( port, () => {
   console.log(`Server running on port ${port}`);
